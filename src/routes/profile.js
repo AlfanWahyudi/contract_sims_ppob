eb0783c532356682ec347ad8dc4f003d1d62cda5
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-02-06 15:23:13 +0700

    feat: update profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -51,7 +51,7 @@ router.get('/', authMiddleware, async (req, res, next) => {
       statusCode = 200
       resJson.status = 0
       resJson.message = "Sukses"
-      resJson.data = userItems[0]
+      resJson.data = {...userItems[0]}
     }
   } catch (error) {
     statusCode = 500
@@ -63,8 +63,114 @@ router.get('/', authMiddleware, async (req, res, next) => {
   return res.status(statusCode).json(resJson)
 })
 
-router.put('/update', authMiddleware, (req, res, next) => {
- //TODO:
+router.put('/update', authMiddleware, async (req, res, next) => {
+  let statusCode = 200
+  const resJson = {
+    status: 0,
+    message: "",
+    data: null,
+  }
+
+  try {
+    const userSchema = Joi.object({
+      first_name: Joi.string()
+        .trim()
+        .max(255)
+        .required()
+        .messages({
+          'string.base': `first_name seharusnya bertipe 'text'`,
+          'string.empty': `first_name tidak boleh kosong`,
+          'string.max': `Panjang first_name tidak boleh melebihi {#limit} karakter`,
+          'any.required': `first_name is required field`
+        }),
+  
+      last_name: Joi.string()
+        .trim()
+        .max(255)
+        .messages({
+          'string.base': `last_name seharusnya bertipe 'text'`,
+          'string.max': `Panjang last_name tidak boleh melebihi {#limit} karakter`,
+          'any.required': `last_name is required field`
+        }),
+    })
+  
+    await userSchema.validateAsync(req.body)
+
+    const userItems = await sequelize.query(
+      `
+        SELECT 
+          email, 
+          first_name, 
+          (
+            CASE
+              WHEN last_name IS NULL THEN ''
+              ELSE last_name
+            END
+          ) AS last_name,
+          (
+            CASE
+              WHEN img_file_name IS NULL THEN ''
+              ELSE concat(:imgPathUrl, img_file_name)
+            END
+          ) AS profile_img
+        FROM 
+          users 
+        WHERE 
+          email = :email
+      `,
+      {
+        replacements: {
+          imgPathUrl: GeneratorUtil.img_path_url(),
+          email: req.userData.email
+        },
+        type: QueryTypes.SELECT
+      }
+    )
+
+    if (userItems.length > 0) {
+      const [results, metadata] = await sequelize.query(
+        `
+          UPDATE users
+          SET first_name = :first_name, last_name= :last_name
+          WHERE email = :email;
+        `,
+        {
+          replacements: {
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.userData.email
+          },
+          type: QueryTypes.UPDATE
+        }
+      )
+
+      if (metadata > 0) {
+        statusCode = 200
+  
+        resJson.status = 0
+        resJson.message = "Sukses"
+        resJson.data = {
+          ...userItems[0],
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+        }
+      }
+    }
+
+  
+  } catch (error) {
+    statusCode = 500
+    resJson.status = 500
+    resJson.message = error.message
+    resJson.data = null
+    
+    if (error instanceof Joi.ValidationError) {
+      statusCode = 400
+      resJson.status = 102
+    }
+  }
+  
+  return res.status(statusCode).json(resJson)
 })
 
 router.put('/image', (req, res, next) => {
