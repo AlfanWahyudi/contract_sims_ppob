9df51883f9e3e99e697db2e8ae67d2179141c322
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-02-06 23:54:46 +0700

    add topupValidator class

--- src/validators/topupValidator.js
+++ src/validators/topupValidator.js
@@ -1,7 +1,4 @@
-// const Joi = require('joi')
-
 exports.validateTopup = async(req, res, next) => {
-
   const amount = req.body.top_up_amount
 
   if (
@@ -17,17 +14,4 @@ exports.validateTopup = async(req, res, next) => {
   }
 
   next();
-  
-  // const schema = Joi.object({
-  //   top_up_amount: Joi.number()
-  //     .min(1)
-  //     .required()
-  //     .messages({
-  //       'string.base': `email seharusnya bertipe 'text'`,
-  //       'string.empty': `email tidak boleh kosong`,
-  //       'string.max': `Panjang email tidak boleh melebihi {#limit} karakter`,
-  //       'any.required': `email is required field`
-  //     }),
-      
-  // })
 }
