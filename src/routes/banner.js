4863667481e886f2da5ffaab716c628d35e86085
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-02-06 12:29:19 +0700

    error handling if failed get all banner and service data

--- src/routes/banner.js
+++ src/routes/banner.js
@@ -5,21 +5,37 @@ const { QueryTypes } = require('sequelize')
 const authMiddleware = require('../middlewares/auth-middleware')
 
 router.get('/', authMiddleware, async (req, res, next) => {
-  //TODO: handle if error while getting data 
-  const bannerItems = await sequelize.query(
-    "SELECT banner_name, banner_image, description FROM banners ORDER BY banner_name asc",
-    {
-      type: QueryTypes.SELECT
-    }
-  )
+  let statusCode = 200
+  const resJson = {
+    status: 0,
+    message: "",
+    data: null
+  }
 
-  if (bannerItems.length > 0) {
-    return res.status(200).json({
-      status: 0,
-      message: "Sukses",
-      data: bannerItems,
-    })    
-  }   
+  try {
+    const bannerItems = await sequelize.query(
+      "SELECT banner_name, banner_image, description FROM banners ORDER BY banner_name asc",
+      {
+        type: QueryTypes.SELECT
+      }
+    )
+  
+    if (bannerItems.length > 0) {
+      statusCode = 200
+
+      resJson.status = 0
+      resJson.message = "Sukses"
+      resJson.data = bannerItems
+    }
+  } catch (error) {
+    statusCode = 500
+
+    resJson.status = 500
+    resJson.message = error.message
+    resJson.data = null
+  }
+
+  return res.status(statusCode).json(resJson)
 })
 
 module.exports = router
--- src/routes/service.js
+++ src/routes/service.js
@@ -5,21 +5,37 @@ const { QueryTypes } = require('sequelize')
 const authMiddleware = require('../middlewares/auth-middleware')
 
 router.get('/', authMiddleware, async (req, res, next) => {
-  //TODO: handle if error while getting data 
-  const serviceItems = await sequelize.query(
-    "SELECT * FROM services ORDER BY service_code asc",
-    {
-      type: QueryTypes.SELECT
-    }
-  )
+  let statusCode = 200
+  const resJson = {
+    status: 0,
+    message: "",
+    data: null
+  }
 
-  if (serviceItems.length > 0) {
-    return res.status(200).json({
-      status: 0,
-      message: "Sukses",
-      data: serviceItems,
-    })    
-  }   
+  try {
+    const serviceItems = await sequelize.query(
+      "SELECT * FROM services ORDER BY service_code asc",
+      {
+        type: QueryTypes.SELECT
+      }
+    )
+  
+    if (serviceItems.length > 0) {
+      statusCode = 200
+
+      resJson.status = 0
+      resJson.message = "Sukses"
+      resJson.data = serviceItems
+    }   
+  } catch (error) {
+    statusCode = 500
+
+    resJson.status = 500
+    resJson.message = error.message
+    resJson.data = null
+  }
+
+  return res.status(statusCode).json(resJson)
 })
 
 module.exports = router
