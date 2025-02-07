fc767a7bbdbb682f4222c48d85bb46ad7c635d24
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-02-07 11:07:48 +0700

    update db connection

--- src/database/database-config.js
+++ src/database/database-config.js
@@ -3,6 +3,8 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false,
+  port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL,
 })
 
 module.exports = { sequelize }
