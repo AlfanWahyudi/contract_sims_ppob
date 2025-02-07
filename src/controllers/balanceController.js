5636e95aa2030b498c1ffc725fcb1b889824ed61
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-02-07 19:12:23 +0700

    update code  for  get user balance

--- src/controllers/balanceController.js
+++ src/controllers/balanceController.js
@@ -1,29 +1,24 @@
 const { sequelize } = require('../database/dbConnection')
 const { QueryTypes } = require('sequelize')
 
+
+const { getUserByEmail } = require('../services/userService')
+
 exports.getBalance = async (req, res) => {
   try {
-    const userItems = await sequelize.query(
-      "SELECT balance FROM users WHERE email = :email",
-      {
-        replacements: {
-          email: req.userData.email
-        },
-        type: QueryTypes.SELECT
-      }
-    )
+    const user = await getUserByEmail(req.userData.email) 
 
-    if (userItems.length > 0) {
-      return res.status(200).json({
-        status: 0,
-        message: "Sukses",
-        data: [...userItems.map(user => 
-          (
-            { balance: parseInt(user.balance) }
-          )
-        )],
-      })
+    if (user === null) {
+      throw new Error("Email tidak ditemukan")
     }
+
+    return res.status(200).json({
+      status: 0,
+      message: "Get Balance Berhasil",
+      data: {
+        balance: parseInt(user.balance)
+      }
+    })
   } catch (error) {
     return res.status(500).json({
       status: 500,
